c264660c825d414118289987e68039e1c578dc5e
BryonVerdone <bryonverdone@gmail.com>
2021-11-15 19:59:14 -0500

    added JS nav functionality

---
+++ app.js
@@ -0,0 +1,7 @@
+const navbar = document.querySelector('.navbar');
+
+const navBtn = document.querySelector('.nav-btn');
+
+navBtn.addEventListener('click', function () {
+  navbar.classList.toggle('show-nav');
+});
